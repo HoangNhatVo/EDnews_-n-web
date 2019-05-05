39d3abee56d766e902b29d4dd60b24871c5da930
Nguyễn Văn Nhật <1612457@student.hcmus.edu.vn>
2019-05-05 13:03:25 +0700

    edit code page subscriber

---
+++ ednews/common/subscriber.js
@@ -0,0 +1,17 @@
+document.getElementById('Ho').value = "Văn Công";
+document.getElementById('Ten').value = "Anh";
+document.getElementById('date').value = "5";
+document.getElementById('month').value = "4";
+document.getElementById('year').value = "1998";
+document.getElementById('Email').value = "EDnews@gmail.com";
+document.getElementById('HSD').value = "30/06/2019";
+
+function ChangePassEmail(){
+	alert("Chưa thể thay đổi mật khẩu!");
+}
+function ReNewedAcc(){
+	alert("Chưa thể gia hạn tài khoản!");
+}
+function ClickButtonSave(){
+	alert("Click Button Save!");
+}
