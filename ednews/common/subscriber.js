e3b62513f9ad28f5ceb117210b925a2327a1025b
NHẬT VÕ HOÀNG <1612462@student.hcmus.edu.vn>
2019-05-05 15:39:02 +0700

    litte edit

--- ednews/common/subscriber.js
+++ ednews/common/subscriber.js
@@ -6,9 +6,7 @@ document.getElementById('year').value = "1998";
 document.getElementById('Email').value = "EDnews@gmail.com";
 document.getElementById('HSD').value = "30/06/2019";
 
-function ChangePassEmail(){
-	alert("Chưa thể thay đổi mật khẩu!");
-}
+
 function ReNewedAcc(){
 	alert("Chưa thể gia hạn tài khoản!");
 }
